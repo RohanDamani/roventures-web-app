aa65833ffaeb53b402f3a511f15ab8fbdea1671e
RohanDamani <damanirohan@gmail.com>
2018-04-29 19:55:49 -0700

    update about text

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ class About extends React.Component {
               <p>{ABOUT.P_2}</p>
               <p>{ABOUT.P_3}</p>
               <p>{ABOUT.P_4}</p>
-              {/*<p>{ABOUT.P_5}</p>*/}
+              <p>{ABOUT.P_5}</p>
             </Col>
             <Col
               xs={10}
@@ -24,7 +24,7 @@ class About extends React.Component {
               <p>{ABOUT.P_2}</p>
               <p>{ABOUT.P_3}</p>
               <p>{ABOUT.P_4}</p>
-              {/*<p>{ABOUT.P_5}</p>*/}
+              <p>{ABOUT.P_5}</p>
             </Col>
             <Col md={2} mdOffset={1} className="text-left hidden-xs hidden-sm">
               <Button
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,7 +19,7 @@ export const VIEWER = {
 export const ABOUT = {
     WEBSITE: 'rohandamani.com',
     LINK: 'http://rohandamani.com',
-    P_1: 'This is the channel for Roventures. I am planning to sail around the world - my photos, videos, location, and crew requests will be shared here.  When I return, I will pursue other interests - giving back, flying small planes, and maybe even hot air ballooning.',
+    P_1: 'This is the channel for Roventures. I am planning to sail around the world - my photos, videos, location, and crew requests will be updated here.',
     P_2: 'I wanted a way to store and share my photos and videos, with the ability to control all aspects, no ads, and the cheapest storage rates, so I built this serverless storage viewer.',
     P_3: 'The viewer itself is a mobile-responsive single page application (SPA) built with JavaScript (ReactJS - React Router, Redux, Thunk, et al). The SPA is hosted on AWS S3 as a static website, using Amazon Cognito to authenticate to a S3 bucket containing all the media.',
     P_4: 'The core of RoVentures is to share the experience and knowledge gained from traveling.  If you would like to contribute, you may contact me directly at damanirohan@gmail.com.',
