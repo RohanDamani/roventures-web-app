5f38bd590beba6d4b39b20f9fd34dab517acf8ce
RohanDamani <damanirohan@gmail.com>
2018-05-04 19:10:31 -0700

    update copy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -25,5 +25,5 @@ export const ABOUT = {
     P_2: 'I wanted a way to store and share my photos and videos, with the ability to control all aspects, no ads, and the cheapest storage rates, so I built this serverless storage viewer.',
     P_3: 'The viewer itself is a mobile-responsive single page application (SPA) built with JavaScript (ReactJS - React Router, Redux, Thunk, et al). The SPA is hosted on AWS S3 as a static website, using Amazon Cognito to authenticate to a S3 bucket containing all the media.',
     P_4: 'The core of RoVentures is to share the experience and knowledge gained from traveling.  If you would like to contribute, you may contact me at info@roventures.tv',
-    P_5: 'Add your email (pending - thanks for checking out this early version!) below to participate in RoVentures, and receive occasional updates on new uploads and sailing opportunities.',
+    P_5: 'Add your email to participate in RoVentures, and receive occasional updates on new uploads and sailing opportunities.',
 }
