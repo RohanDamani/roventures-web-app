eda5d800dece9076850cbade8881f09886b55f82
RohanDamani <damanirohan@gmail.com>
2018-05-04 19:19:59 -0700

    update copy and css

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,7 +23,7 @@ export const ABOUT = {
     LINK: 'http://rohandamani.com',
     P_1: 'Welcome to the channel for RoVentures. I am planning to sail around the world - my photos, videos, location, and crew requests will be updated here.',
     P_2: 'I wanted a way to store and share my photos and videos, with the ability to control all aspects, no ads, and the cheapest storage rates, so I built this serverless storage viewer.',
-    P_3: 'The viewer itself is a mobile-responsive single page application (SPA) built with JavaScript (ReactJS - React Router, Redux, Thunk, et al). The SPA is hosted on AWS S3 as a static website, using Amazon Cognito to authenticate to a S3 bucket containing all the media.',
+    P_3: 'The viewer itself is a mobile-responsive SPA built with JavaScript - ReactJS, React Router, Redux, Thunk, et al. The SPA is hosted with Route53 on S3 as a static website, using Amazon Cognito to authenticate to a S3 bucket containing all the media.  The email capture is a custom serverless solution, using Lambda, SES, and DynamoDB',
     P_4: 'The core of RoVentures is to share the experience and knowledge gained from traveling.  If you would like to contribute, you may contact me at info@roventures.tv',
     P_5: 'Add your email to participate in RoVentures, and receive occasional updates on new uploads and sailing opportunities.',
 }
